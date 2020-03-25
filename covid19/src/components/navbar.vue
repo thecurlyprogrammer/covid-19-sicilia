80187bc160220aa7854fbf954bcb33ea29126e5f
Sandro Greco <grecosandro@outlook.com>
2020-03-25 17:50:02 +0100

    Modifica al link autocertificazione

--- covid19/src/components/navbar.vue
+++ covid19/src/components/navbar.vue
@@ -17,7 +17,7 @@
         <!-- Right aligned nav items -->
         <b-navbar-nav class="ml-auto">
           <b-nav-item href="http://covid19.sandrogreco.com/modello/autodichiarazione.pdf"
-          target="blank" download>Modello Autodichiarazione</b-nav-item>
+          target="_blank" download>Modello Autodichiarazione</b-nav-item>
         </b-navbar-nav>
       </b-collapse>
     </b-navbar>
